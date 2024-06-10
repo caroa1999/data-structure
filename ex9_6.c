575af5bd4257897d13fd260c59e0d318e579730f
caroa1999 <162245193+caroa1999@users.noreply.github.com>
2024-06-10 19:14:06 +0900

    ch09_6 (과제 6) (Sort)

---
+++ ex9_6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void shellSort(int a[], int size);
+
+int main(void) {
+	int list[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
+	int size = sizeof(list) / sizeof(list[0]);
+	printf("\n정렬할 원소 : ");
+	printf("\n\n <<< 셀 정렬 수행 >>>\n");
+	shellSort(list, size);
+
+	getchar(); return 0;
+}
